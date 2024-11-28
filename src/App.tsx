ca8088e77351204e1db3cf8de55a2567a50c43ca
Simabhagat <moirangthembhagat338@gmail.com>
2024-11-28 19:12:25 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromChildren, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
 import RootLayout from './layout/RootLayout.tsx'
 import HomeLayout from './layout/HomeLayout.tsx'
